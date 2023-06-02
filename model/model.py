ecb7861bad8d2c27f5cd662d0324a9824e3459c9
Artyom Khrennikov <artyomkhrenn@gmail.com>
2023-06-02 21:41:10 +0500

    review_changes

--- model/model.py
+++ model/model.py
@@ -3,12 +3,11 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.yaml")  # build a new model from scratch
 
 # Use the model
-model.train(data='F:\\computer_vision\\yolo\\config.yaml', epochs=29,   
+model.train(data='config.yaml', epochs=29,   
     optimizer='Adam',
     val=True,
     batch=64,
-    imgsz=640)  # train the model
+    imgsz=640,
+    weight_name = 'model')  # train the model
 metrics = model.val()  # evaluate model performance on the validation set
 
-# predict on an image
-results = model("C://Users//Artyom//Downloads//HGIC_spiders_wolfspider_16x9.jpg")
