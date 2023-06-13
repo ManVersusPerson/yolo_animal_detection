38c51840d2b96f654862b24adc7d126462ad86b0
Akashy <jen.danilow2013@yandex.ru>
2023-06-13 21:56:25 +0500

    The first version of the tests

---
+++ test_main.py
@@ -0,0 +1,28 @@
+from fastapi.testclient import TestClient
+from api.api import app
+
+client = TestClient(app)
+
+
+# def test_upload_image_file_mode():
+#     filename = "./img_test/Fish.jpg"
+#     files = {"file": (open(filename, "rb"))}
+#     response = client.post(
+#         "/predict/img", files=files)
+#     assert response.status_code == 200
+
+
+def test_upload_image_json_mode():
+    filename = "./img_test/Fish.jpg"
+    files = {"file": (open(filename, "rb"))}
+    response = client.post(
+        "/predict/json", files=files)
+    assert response.status_code == 200
+
+
+# def test_image_analysis():
+#     filename = "./img_test/Fish.jpg"
+#     files = {"file": (open(filename, "rb"))}
+#     response = client.post(
+#         "/predict/img", files=files)
+#     assert response.status_code == 200
