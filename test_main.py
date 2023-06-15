1ab1b42f4d1a864749c0d91fcfbef4ea5f2e1940
Akashy <jen.danilow2013@yandex.ru>
2023-06-15 13:14:42 +0500

    Add the first version test

--- test_main.py
+++ test_main.py
@@ -4,25 +4,19 @@ from api.api import app
 client = TestClient(app)
 
 
-# def test_upload_image_file_mode():
-#     filename = "./img_test/Fish.jpg"
-#     files = {"file": (open(filename, "rb"))}
-#     response = client.post(
-#         "/predict/img", files=files)
-#     assert response.status_code == 200
+def test_upload_image_file_mode():
+    filename = "./img_test/Fish.jpg"
+    files = {"file": (open(filename, "rb"))}
+    response = client.post(
+        "/predict/img", files=files)
+    assert response.status_code == 200
 
 
 def test_upload_image_json_mode():
-    filename = "yolo_animal_detection\FISH.jpg"
+    filename = "./img_test/Fox.jpg"
     files = {"file": (open(filename, "rb"))}
     response = client.post(
         "/predict/json", files=files)
     assert response.status_code == 200
 
 
-# def test_image_analysis():
-#     filename = "./img_test/Fish.jpg"
-#     files = {"file": (open(filename, "rb"))}
-#     response = client.post(
-#         "/predict/img", files=files)
-#     assert response.status_code == 200
