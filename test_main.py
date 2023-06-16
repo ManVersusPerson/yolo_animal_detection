4833aa167937da09689f1409b2398dceea38b617
Evgeniy <69479025+ExtensiveTree@users.noreply.github.com>
2023-06-16 10:21:21 +0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -2,19 +2,14 @@ from fastapi.testclient import TestClient
 from api.api import app
 
 client = TestClient(app)
+test_filename = ("./img_test/Fish.jpg", "./img_test/Fox.jpg")
+ROUTES = ('/predict/img', '/predict/json',)
 
 
-def test_upload_image_file_mode():
-    filename = "./img_test/Fish.jpg"
-    files = {"file": (open(filename, "rb"))}
-    response = client.post(
-        "/predict/img", files=files)
-    assert response.status_code == 200
-
-
-def test_upload_image_json_mode():
-    filename = "./img_test/Fox.jpg"
-    files = {"file": (open(filename, "rb"))}
-    response = client.post(
-        "/predict/json", files=files)
-    assert response.status_code == 200
+def test_upload_image():
+    for filename in test_filename:
+        for routes in ROUTES:
+            files = {"file": (open(filename, "rb"))}
+            response = client.post(
+                routes, files=files)
+            assert response.status_code == 200
