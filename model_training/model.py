16e3aba1aa0c5b42de538bf605706cb3311ba7f4
Evgeniy <69479025+ExtensiveTree@users.noreply.github.com>
2023-06-16 09:42:33 +0500

    Update model.py

--- model_training/model.py
+++ model_training/model.py
@@ -3,12 +3,11 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.yaml")  # build a new model from scratch
 
 # Use the model
-model.train(data='config.yaml', epochs=29,   
+model.train(data='config.yaml', epochs=29,
     optimizer='Adam',
     val=True,
     batch=64,
     imgsz=640,
-    weight_path = '../model',
-    weight_name = 'model')  # train the model
+    weight_path='../model',
+    weight_name='model')  # train the model
 metrics = model.val()  # evaluate model performance on the validation set
-
